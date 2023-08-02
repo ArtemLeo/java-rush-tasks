8e3efd830b0eecdd145aa078af8b8bbf0acfc870
Artem Andriushchenko <link.by.artem@gmail.com>
2023-08-02 16:52:11 +0300

    add Task_013 014 015

---
+++ src/Level_2_Array/Task_013_Array.java
@@ -0,0 +1,15 @@
+package Level_2_Array;
+
+// Выводим массив;
+
+import java.util.Arrays;
+
+public class Task_013_Array {
+    public static String[] strings = new String[]{"I", "love", "Java"};
+    public static int[] ints = new int[]{1, 2, 3, 4, 5, 6, 7, 8};
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(strings));
+        System.out.println(Arrays.toString(ints));
+    }
+}
