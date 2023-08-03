ac3f58b3e0cdd1023170aa548204e74fe60b5758
Artem Andriushchenko <link.by.artem@gmail.com>
2023-08-03 04:12:31 +0300

    add Task_018

---
+++ src/Level_2_Array/Task_018_Array.java
@@ -0,0 +1,15 @@
+package Level_2_Array;
+
+// Сортировка массива;
+
+import java.util.Arrays;
+
+public class Task_018_Array {
+    public static int[] array = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+
+    public static void main(String[] args) {
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
+
