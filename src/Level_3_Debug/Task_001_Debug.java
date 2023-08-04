1a167b6a00e327e57f0f675845fc72a5b5776cf2
Artem Andriushchenko <link.by.artem@gmail.com>
2023-08-04 06:28:23 +0300

    add Task_001_Debug 002

---
+++ src/Level_3_Debug/Task_001_Debug.java
@@ -0,0 +1,16 @@
+package Level_3_Debug;
+
+// Переобуваемся на лету;
+
+public class Task_001_Debug {
+    public static int result = 105;
+
+    public static void main(String[] args) {
+        byte correction = Byte.MAX_VALUE;
+        for (double fahrenheit = -459.67; fahrenheit < 451; fahrenheit += 40) {
+            correction *= fahrenheit;
+            System.out.println(correction);
+        }
+    }
+}
+
