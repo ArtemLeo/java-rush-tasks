5c7aea6ed7d42da4b9fd7b7f725c72ac81078f62
Artem Andriushchenko <link.by.artem@gmail.com>
2023-08-05 04:39:36 +0300

    add Task_006_Methods

---
+++ src/Level_4_Methods/Task_006_Methods.java
@@ -0,0 +1,49 @@
+package Level_4_Methods;
+
+// Универсальный солдат;
+
+public class Task_006_Methods {
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    public static void universalMethod(int a){
+
+    }
+
+    public static void universalMethod(int a, int b){
+
+    }
+
+    public static void universalMethod(double a){
+
+    }
+
+    public static void universalMethod(double a, double b){
+
+    }
+
+    public static void universalMethod(char a, char b){
+
+    }
+
+    public static void universalMethod(String a, String b){
+
+    }
+
+    public static void universalMethod(boolean a){
+
+    }
+
+    public static void universalMethod(boolean a, boolean b){
+
+    }
+
+    public static void universalMethod(long a){
+
+    }
+}
