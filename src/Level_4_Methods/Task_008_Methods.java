1b877f265c044319afd5404ccdc7ef2eeb787b8a
Artem Andriushchenko <link.by.artem@gmail.com>
2023-08-05 04:54:16 +0300

    add Task_008_Methods

---
+++ src/Level_4_Methods/Task_008_Methods.java
@@ -0,0 +1,14 @@
+package Level_4_Methods;
+
+// Кубический калькулятор;
+
+public class Task_008_Methods {
+    public static void main(String[] args) {
+
+    }
+
+    public static long cube(long a) {
+        return a * a * a;
+    }
+}
+
