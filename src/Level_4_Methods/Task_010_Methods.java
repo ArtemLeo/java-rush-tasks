1c2af180a22485020967b6a49d1689e1ebc35a4f
Artem Andriushchenko <link.by.artem@gmail.com>
2023-08-05 09:34:12 +0300

    add Task_010_Methods

---
+++ src/Level_4_Methods/Task_010_Methods.java
@@ -0,0 +1,35 @@
+package Level_4_Methods;
+
+// Борьба за доступ;
+
+public class Task_010_Methods {
+    public static void main(String[] args) {
+        Person person = new Person("Иван", "Иванов");
+        System.out.println("Досье.");
+        System.out.println("Имя: " + person.getFirstName());
+        System.out.println("Фамилия: " + person.getLastName());
+        System.out.println("Полное имя: " + person.getFullName());
+    }
+}
+class Person {
+    public String firstName;
+    public String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    protected String getFirstName() {
+        return firstName;
+    }
+
+    String getLastName() {
+        return lastName;
+    }
+
+    public String getFullName(){
+        return firstName + " " + lastName;
+    }
+}
+
