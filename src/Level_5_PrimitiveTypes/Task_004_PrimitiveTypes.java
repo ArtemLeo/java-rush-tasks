5a4222ff2eb8dac0d661925552af7b7b112dadd3
Artem Andriushchenko <link.by.artem@gmail.com>
2023-08-07 03:18:16 +0300

    add Task_004_PrimitiveTypes 005

--- src/Level_5_PrimitiveTypes/Task_004_PrimitiveTypes.java
+++ src/Level_5_PrimitiveTypes/Task_004_PrimitiveTypes.java
@@ -6,9 +6,17 @@ public class Task_004_PrimitiveTypes {
     public static void main(String[] args) {
         div(0.0, 10);
         div(0.0, -10);
+        System.out.println("--------------------");
+
+        div(0, 1);
+        div(0, -1);
     }
 
     public static void div(double a, double b) {
+        System.out.println(b / a);
+    }
+
+    public static void div(float a, float b) {
         System.out.println(b / a);
     }
 }
---
+++ src/Level_5_PrimitiveTypes/Task_005_PrimitiveTypes.java
@@ -0,0 +1,13 @@
+package Level_5_PrimitiveTypes;
+
+// Бесконечность не предел;
+
+public class Task_005_PrimitiveTypes {
+        public static void main(String[] args) {
+            div(0.0,0.0);
+        }
+
+        public static void div(double a, double b){
+            System.out.println(b/a);
+        }
+}
