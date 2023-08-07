21975dd92621442638a5e125018e38cf764230f5
Artem Andriushchenko <link.by.artem@gmail.com>
2023-08-07 03:52:48 +0300

    add Task_009_PrimitiveTypes

---
+++ src/Level_5_PrimitiveTypes/Task_009_PrimitiveTypes.java
@@ -0,0 +1,20 @@
+package Level_5_PrimitiveTypes;
+
+// Массив значений;
+
+import java.util.Arrays;
+
+public class Task_009_PrimitiveTypes {
+    public static void main(String[] args) {
+        Integer[] array = new Integer[5];
+        fillArray(array, 3);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void fillArray(Integer[] array, int value) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] = value;
+        }
+    }
+}
+
