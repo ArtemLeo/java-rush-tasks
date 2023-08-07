c781165fcf166b562c62b1d4e83af6e90f85ebc6
Artem Andriushchenko <link.by.artem@gmail.com>
2023-08-07 03:41:21 +0300

    add Task_008_PrimitiveTypes

---
+++ src/Level_5_PrimitiveTypes/Task_008_PrimitiveTypes.java
@@ -0,0 +1,11 @@
+package Level_5_PrimitiveTypes;
+
+// Очень правильные типы;
+
+public class Task_008_PrimitiveTypes {
+    long a = 109 + 15;
+    int b = (int) a * 2;
+    short c = (short) (a / b);
+    byte d = (byte) (a + b - c);
+}
+
