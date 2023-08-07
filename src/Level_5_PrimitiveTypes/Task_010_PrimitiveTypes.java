7031b0e31d36ef22af0cde2f046ab2029283e105
Artem Andriushchenko <link.by.artem@gmail.com>
2023-08-07 04:09:01 +0300

    add Task_010_PrimitiveTypes

---
+++ src/Level_5_PrimitiveTypes/Task_010_PrimitiveTypes.java
@@ -0,0 +1,20 @@
+package Level_5_PrimitiveTypes;
+
+// Гибкое заполнение массива;
+
+import java.util.Arrays;
+
+public class Task_010_PrimitiveTypes {
+    public static void main(String[] args) {
+        Integer[] array = new Integer[5];
+        fillArray(array, 3, 1, 3);
+        System.out.println(Arrays.toString(array));
+    }
+
+    public static void fillArray(Integer[] array, int value, int begin, int end) {
+        for (int i = begin; i < end; i++) {
+            array[i] = value;
+        }
+    }
+}
+
